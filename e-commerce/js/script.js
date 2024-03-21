00cf4b49f6232c07dab953589d027f2adcaa3446
Eleshin <awwaleleshin@gmail.com>
2024-03-21 22:01:03 +0100

    popular category

--- e-commerce/js/script.js
+++ e-commerce/js/script.js
@@ -1,1 +1,25 @@
-console.log('Hello World!');
+// Swiper categories
+var swiperCAtegories = new Swiper(".category-container", {
+    spaceBetween: 24,
+    loop:true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 24,
+        },
+    },
+});
