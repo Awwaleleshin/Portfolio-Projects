cbf6b366242bc4d777e302fb01d2802b88474576
Eleshin <awwaleleshin@gmail.com>
2024-05-20 15:11:12 +0100

    swiper js pagination

--- Burger website/src/js/main.js
+++ Burger website/src/js/main.js
@@ -64,9 +64,30 @@ tabs.forEach(tab => {
 
 // Swiper JS
 const swiper = new Swiper('.swiper', {
-  
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
+    grabCursor: true,
+    breakpoints: {
+        // when window width is >= 320px
+        640: {
+          slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        768: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        1024: {
+          slidesPerView: 3,
+        }
+      }
   });
