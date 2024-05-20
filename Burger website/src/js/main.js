411329c70d63182f279d87b0fb0e4c4b1933bc70
Eleshin <awwaleleshin@gmail.com>
2024-05-20 17:28:29 +0100

    theme toogle

--- Burger website/src/js/main.js
+++ Burger website/src/js/main.js
@@ -91,3 +91,33 @@ const swiper = new Swiper('.swiper', {
         }
       }
   });
+
+//   Dark light theme
+const html = document.querySelector('html');
+const themeBtn = document.getElementById('theme-toogle');
+
+function darkMode () {
+    html.classList.add('dark');
+    themeBtn.classList.replace('ri-moon-line', 'ri-sun-line');
+    localStorage.setItem('mode', 'dark');
+}
+
+function lightMode () {
+    html.classList.remove('dark');
+    themeBtn.classList.replace('ri-sun-line', 'ri-moon-line');
+    localStorage.setItem('mode', 'light');
+}
+
+if (localStorage.getItem('mode') == 'dark') {
+    darkMode();
+}   else {
+    lightMode()
+}
+
+themeBtn.addEventListener('click', (e) => {
+    if (localStorage.getItem('mode')  == 'light') {
+        darkMode();
+    } else {
+        lightMode()
+    }
+})
