b5ef82288a7fe8fa099f05990a26e016d8d721f7
Eleshin <awwaleleshin@gmail.com>
2024-05-31 21:50:58 +0100

    show full description option

--- react-jobs/src/components/JobListing.jsx
+++ react-jobs/src/components/JobListing.jsx
@@ -5,6 +5,7 @@ const JobListing = ({job}) => {
   const [showFullDescription, setShowFullDescription] =useState(false);
 
   let description = job.description
+
   if (!showFullDescription) {
     description = description.substring(0, 90) + '...'
   }
@@ -18,6 +19,8 @@ const JobListing = ({job}) => {
             </div>
             
                 <div className="mb-5">{description}</div>
+
+                <button className='text-indigo-500 mb-5 hover:text-indigo-600'>{showFullDescription ? 'Less' : 'More'}</button>
             
                 <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
             
