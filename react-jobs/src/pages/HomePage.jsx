cfd1a33d4305dc635fcf38444ad9d0522d998326
Eleshin <awwaleleshin@gmail.com>
2024-06-04 08:59:13 +0100

    homepage

--- react-jobs/src/pages/HomePage.jsx
+++ react-jobs/src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+import JobListings from '../components/JobListings'
+import JobListing from '../components/JobListing'
+import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
   return (
     <>
         <Hero />
+        <HomeCards />
+        <JobListings />
+        <ViewAllJobs />
     </>
   )
 }
