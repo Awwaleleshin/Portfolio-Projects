49bc6ed268c346724286807d96a91486b575d162
Eleshin <awwaleleshin@gmail.com>
2024-06-09 07:30:52 +0100

    job listing

--- react-jobs/src/pages/JoPage.jsx
+++ react-jobs/src/pages/JoPage.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const JoPage = () => {
+    const { id } = useParams()
+    const [job, setJob] = useState(null)
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(`/api/job/${id}`);
+                const data = await res.json();
+                setJob(data)
+              } catch (error) {
+                console.log('Error fetching data', error)
+              } finally {
+                setLoading(false)
+              }
+        }
+
+        fetchJob()
+    }, [])
+
   return (
     <div>
       
