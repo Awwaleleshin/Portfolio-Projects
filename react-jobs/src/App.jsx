8757ed1971865ad25abed06919b7305727cdba54
Eleshin <awwaleleshin@gmail.com>
2024-06-09 07:13:56 +0100

    single page setup

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -7,12 +7,14 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobPage from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JoPage from './pages/JoPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/'element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='jobs' element={<JobPage />} />
+    <Route path='jobs/:id' element={<JoPage />} />
     <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
