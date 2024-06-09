e452fb2b12c7db77388939d6a71a96821143eca0
Eleshin <awwaleleshin@gmail.com>
2024-06-09 07:39:15 +0100

    getting ID with useParams()

--- react-jobs/src/pages/JoPage.jsx
+++ react-jobs/src/pages/JoPage.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner'
 
 const JoPage = () => {
     const { id } = useParams()
     const [job, setJob] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchJob = async () => {
             try {
-                const res = await fetch(`/api/job/${id}`);
+                const res = await fetch(`/api/jobs/${id}`);
                 const data = await res.json();
                 setJob(data)
               } catch (error) {
@@ -21,10 +23,8 @@ const JoPage = () => {
         fetchJob()
     }, [])
 
-  return (
-    <div>
-      
-    </div>
+  return loading ? <Spinner /> : (
+    <h1>{job.title}</h1>
   )
 }
 
