1cbc69766d706a22084c356d78ab1f59b839dec0
Eleshin <awwaleleshin@gmail.com>
2024-06-10 20:57:11 +0100

    data loader

--- react-jobs/src/pages/JoPage.jsx
+++ react-jobs/src/pages/JoPage.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 const JoPage = () => {
     const { id } = useParams()
-    const [job, setJob] = useState(null)
-    const [loading, setLoading] = useState(true)
+    // const [job, setJob] = useState(null)
+    // const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data)
-              } catch (error) {
-                console.log('Error fetching data', error)
-              } finally {
-                setLoading(false)
-              }
-        }
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`);
+    //             const data = await res.json();
+    //             setJob(data)
+    //           } catch (error) {
+    //             console.log('Error fetching data', error)
+    //           } finally {
+    //             setLoading(false)
+    //           }
+    //     }
 
-        fetchJob()
-    }, [])
+    //     fetchJob()
+    // }, [])
 
   return loading ? <Spinner /> : (
     <h1>{job.title}</h1>
   )
 }
 
-export default JoPage
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`)
+    const data = await res.json()
+}
+
+export { JoPage as default, jobLoader}
