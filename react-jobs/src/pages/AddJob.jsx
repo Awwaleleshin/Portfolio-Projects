cb661dcca807e7578e0a156d472fafc6d1122691
Eleshin <awwaleleshin@gmail.com>
2024-06-12 21:47:06 +0100

    add submitted job

--- react-jobs/src/pages/AddJob.jsx
+++ react-jobs/src/pages/AddJob.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const AddJob = () => {
+const AddJob = ({ addJobSubmit }) => {
   const [title, setTitle] = useState('');
   const [type, setType] = useState('Full-Time');
   const [location, setLocation] = useState('');
@@ -10,6 +11,8 @@ const AddJob = () => {
   const [companyDescription, setCompanyDescription] = useState('');
   const [contactEmail, setContactEmail] = useState('');
   const [contactPhone, setContactPhone] = useState('');
+
+  const navigate = useNavigate
 
   const submitForm = (e) => {
     e.preventDefault()
@@ -27,7 +30,9 @@ const AddJob = () => {
         contactPhone
       } 
     }
-    console.log(newJob)
+    addJobSubmit(newJob)
+
+    return navigate('/jobs')
   }
 
   return (
