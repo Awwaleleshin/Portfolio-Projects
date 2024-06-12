09847e4b1ed5d7df00cb153f6ba92457b386ba2e
Eleshin <awwaleleshin@gmail.com>
2024-06-12 20:57:54 +0100

    working with forms

--- react-jobs/src/pages/AddJob.jsx
+++ react-jobs/src/pages/AddJob.jsx
@@ -30,6 +30,8 @@ const AddJob = () => {
                 name="type"
                 className="border rounded w-full py-2 px-3"
                 required
+                value={type}
+                onChange={(e) => setType(e.target.value)}
               >
                 <option value="Full-Time">Full-Time</option>
                 <option value="Part-Time">Part-Time</option>
@@ -49,6 +51,8 @@ const AddJob = () => {
                 className="border rounded w-full py-2 px-3 mb-2"
                 placeholder="eg. Senior React Developer"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div className="mb-4">
@@ -63,6 +67,8 @@ const AddJob = () => {
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}             
               ></textarea>
             </div>
 
@@ -75,6 +81,8 @@ const AddJob = () => {
                 name="salary"
                 className="border rounded w-full py-2 px-3"
                 required
+                value={salary}
+                onChange={(e) => setSalary(e.target.value)}
               >
                 <option value="Under $50K">Under $50K</option>
                 <option value="$50K - 60K">$50K - $60K</option>
@@ -100,7 +108,9 @@ const AddJob = () => {
                 name='location'
                 className='border rounded w-full py-2 px-3 mb-2'
                 placeholder='Company Location'
-                required           
+                required
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}           
               />
             </div>
 
@@ -116,6 +126,8 @@ const AddJob = () => {
                 name="company"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
               />
             </div>
 
@@ -131,6 +143,8 @@ const AddJob = () => {
                 className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
+                value={companyDescription}
+                onChange={(e) => setCompanyDescription(e.target.value)}
               ></textarea>
             </div>
 
@@ -147,6 +161,8 @@ const AddJob = () => {
                 className="border rounded w-full py-2 px-3"
                 placeholder="Email address for applicants"
                 required
+                value={contactEmail}
+                onChange={(e) => setContactEmail(e.target.value)}
               />
             </div>
             <div className="mb-4">
@@ -161,6 +177,8 @@ const AddJob = () => {
                 name="contact_phone"
                 className="border rounded w-full py-2 px-3"
                 placeholder="Optional phone for applicants"
+                value={contactPhone}
+                onChange={(e) => setContactPhone(e.target.value)}
               />
             </div>
 
