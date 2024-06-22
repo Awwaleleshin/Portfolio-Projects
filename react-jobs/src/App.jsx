94e50adf4c185010c3704d6dc745d30e0d084b40
Eleshin <awwaleleshin@gmail.com>
2024-06-22 23:28:43 +0100

    put request to update job

--- react-jobs/src/App.jsx
+++ react-jobs/src/App.jsx
@@ -32,14 +32,35 @@ const App = () =>{
       return
   }
 
+  // Update Job
+  const updateJob = async (job) =>{
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    })
+    return
+  }
+
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/'element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='jobs' element={<JobPage />} />
-    <Route path='add-job' element={<AddJob addJobSubmit={addJob}/>} />
-    <Route path='edit-job/:id' element={<EditJobPage />} loader={jobLoader}/>
-    <Route path='jobs/:id' element={<JoPage deleteJob={ deleteJob } />} loader={jobLoader}/>
+    <Route 
+      path='add-job' 
+      element={<AddJob addJobSubmit={addJob}/>} 
+    />
+    <Route 
+      path='edit-job/:id' 
+      element={<EditJobPage updateJobSubmit={updateJob}/>} 
+      loader={jobLoader}/>
+    <Route 
+      path='jobs/:id' 
+      element={<JoPage deleteJob={ deleteJob } />} 
+      loader={jobLoader}/>
     <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
