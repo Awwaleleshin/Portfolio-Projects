af623b6c329bc8abb7f36ea05b4e53f6dc3bcd1c
Eleshin <awwaleleshin@gmail.com>
2024-06-22 22:24:06 +0100

    defining use state

--- react-jobs/src/pages/EditJobPage.jsx
+++ react-jobs/src/pages/EditJobPage.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom'
 
 const EditJobPage = () => {
-    const [title, setTitle] = useState('');
-    const [type, setType] = useState('Full-Time');
-    const [location, setLocation] = useState('');
-    const [description, setDescription] = useState('');
-    const [salary, setSalary] = useState('Under $50K');
-    const [companyName, setCompanyName] = useState('');
-    const [companyDescription, setCompanyDescription] = useState('');
-    const [contactEmail, setContactEmail] = useState('');
-    const [contactPhone, setContactPhone] = useState('');
-    
     const job = useLoaderData()
 
+    const [title, setTitle] = useState(job.title);
+    const [type, setType] = useState(job.type);
+    const [location, setLocation] = useState(job.location);
+    const [description, setDescription] = useState(job.description);
+    const [salary, setSalary] = useState(job.salary);
+    const [companyName, setCompanyName] = useState(job.company.name);
+    const [companyDescription, setCompanyDescription] = useState(job.company.description);
+    const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+    const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
+    
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
