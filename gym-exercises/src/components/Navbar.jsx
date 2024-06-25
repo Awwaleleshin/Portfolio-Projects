f022bc259df85e48f3ee4edbc0bb43aea6340f1e
Eleshin <awwaleleshin@gmail.com>
2024-06-25 07:23:36 +0100

    exercise logo

--- gym-exercises/src/components/Navbar.jsx
+++ gym-exercises/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to='/'>
+        <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}}/>
+      </Link>
+    </Stack>
   )
 }
 
