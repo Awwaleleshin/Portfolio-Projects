d2173be29fb015e15ada572d35fc94b921a3d436
Eleshin <awwaleleshin@gmail.com>
2024-06-26 14:32:55 +0100

    hero banner

--- gym-exercises/src/components/HeroBanner.jsx
+++ gym-exercises/src/components/HeroBanner.jsx
@@ -5,18 +5,13 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => {
   return (
-    <Box sx= {{
-      mt: { lg: '212px', xs: '70px'},
-      ml: {  sm: '50px'}
-    }} position='relative'>
+    <Box sx= {{ mt: { lg: '210px', xs: '70px'}, ml: {  sm: '50px'}}} position='relative'>
       
       <Typography color='#FF2625' fontWeight='600' fontSize='26px'>
         Fitness Club
       </Typography>
 
-      <Typography fontWeight='700'
-      sx={{ fontSize: { lg: '44px', xs: '40px'}}}
-      mb='23px' mt='30px'>
+      <Typography fontWeight='700' sx={{ fontSize: { lg: '44px', xs: '40px'}}} mb='23px' mt='30px'>
         Sweat, Smile <br /> and Repeat
       </Typography>
 
@@ -24,13 +19,11 @@ const HeroBanner = () => {
         Check out the most effective exercises
       </Typography>
 
-      <Button variant='contained' color='error' href='#exercises'>Explore Exercises</Button>
+      <Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor: '#FF2625', padding: '10px'}}>
+        Explore Exercises
+      </Button>
 
-      <Typography fontWeight='600' color='#ff2625' 
-        sx={{opacity: 0.1, 
-        display: {lg: 'block', xs: 'none'}
-        }}
-        fontSize='200px'>
+      <Typography fontWeight='600' color='#ff2625' sx={{opacity: 0.1, display: {lg: 'block', xs: 'none'}}} fontSize='200px'>
         Exercise
       </Typography>
 
