26bbda630513d585caaee82d58c8e7b2964052c5
Eleshin <awwaleleshin@gmail.com>
2024-06-27 07:46:22 +0100

    search exercise textfield styling

--- gym-exercises/src/components/SearchExercises.jsx
+++ gym-exercises/src/components/SearchExercises.jsx
@@ -4,12 +4,16 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 const SearchExercises = () => {
   return (
     <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      
       <Typography fontWeight='700' sx={{fontSize: { lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>
         Awesome Exercises You <br /> Should Know
       </Typography>
 
       <Box position='relative' mb='72px'>
-        <TextField height='76px' value='' onChange={(e) => {}} placeholder='Search Exercises' />
+        <TextField sx={{ 
+          input: { fontWeight:'700'},
+          width: { lg: '1170px', xs: '350px'}}} 
+          height='76px' value='' onChange={(e) => {}} placeholder='Search Exercises' type='text' />
       </Box>
     </Stack>
   )
