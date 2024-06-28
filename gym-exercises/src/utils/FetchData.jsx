b16ece6d3400fdac9dc8e7075c0ba3537e165ae0
Eleshin <awwaleleshin@gmail.com>
2024-06-28 19:57:55 +0100

    fetching data

--- gym-exercises/src/utils/FetchData.jsx
+++ gym-exercises/src/utils/FetchData.jsx
@@ -1,3 +1,12 @@
+export const exerciseOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '52360df790msh4c38580ac1eec60p1796bdjsnb569672cae27',
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
