d1b4188f7eeef1e8e99af2b9e65976ec58d8ee92
Eleshin <awwaleleshin@gmail.com>
2024-07-01 07:47:51 +0100

    fixed api connection

--- gym-exercises/src/pages/Home.jsx
+++ gym-exercises/src/pages/Home.jsx
@@ -6,10 +6,13 @@ import SearchExercises from '../components/SearchExercises';
 import Excercises from '../components/Excercises';
 
 const Home = () => {
+  const aaa = process.env.REACT_APP_RAPID_API_KEY
+  console.log(aaa)
   return (
     <Box>
       <HeroBanner />
       <SearchExercises />
+      <p>Code: {import.meta.env}</p>
       <Excercises />
     </Box>
   )
