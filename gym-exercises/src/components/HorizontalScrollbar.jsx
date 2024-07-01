0571d96786e11d7de574ff07d46e68c446706c87
Eleshin <awwaleleshin@gmail.com>
2024-07-01 14:47:51 +0100

    exercise body parts

--- gym-exercises/src/components/HorizontalScrollbar.jsx
+++ gym-exercises/src/components/HorizontalScrollbar.jsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material'
 
 import BodyPart from './BodyPart'
 
-const HorizontalScrollbar = ({ data }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
     <div>
         {data.map((item) => (
@@ -15,7 +15,7 @@ const HorizontalScrollbar = ({ data }) => {
                 <BodyPart 
                 item={item} 
                 bodyPart= {bodyPart} 
-                setBodyPart={setBodyParts}/>
+                setBodyPart={setBodyPart}/>
             </Box>
         ))}
     </div>
