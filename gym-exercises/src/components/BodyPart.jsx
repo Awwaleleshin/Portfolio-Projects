a888459b41e989db1b0d7b1ac4f48b0ee54c8e1b
Eleshin <awwaleleshin@gmail.com>
2024-07-02 05:29:26 +0100

    exercise body parts card

--- gym-exercises/src/components/BodyPart.jsx
+++ gym-exercises/src/components/BodyPart.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Stack, Typography } from '@mui/material'
+
+import icon from '../assets/icons/gym.png'
 
 const BodyPart = () => {
   return (
-    <div>BodyPart</div>
+    <Stack
+        type='button'
+        alignContent='center'
+        alignItems='center'
+        className='bodyPart-card'
+    >
+        <img src={icon} alt="dumbell" style={{width: '40px', height: '40px'}} />
+    </Stack>
   )
 }
 
