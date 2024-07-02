403b109bfe6c88694173178ca389c185e2c162c3
Eleshin <awwaleleshin@gmail.com>
2024-07-02 06:11:40 +0100

    category icons

--- gym-exercises/src/components/BodyPart.jsx
+++ gym-exercises/src/components/BodyPart.jsx
@@ -3,13 +3,22 @@ import { Stack, Typography } from '@mui/material'
 
 import icon from '../assets/icons/gym.png'
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
         type='button'
         alignContent='center'
         alignItems='center'
         className='bodyPart-card'
+        sx={{
+            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+                backgroundColor: '#fff',
+                borderBottomLeftRadius: '20px',
+                width: '270px',
+                height: '280px',
+                cursor: 'pointer',
+                gap: '47px'
+            }}
     >
         <img src={icon} alt="dumbell" style={{width: '40px', height: '40px'}} />
     </Stack>
