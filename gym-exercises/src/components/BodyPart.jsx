028605b54e74250dd283db1ace1fc077af7fd790
Eleshin <awwaleleshin@gmail.com>
2024-07-02 06:17:35 +0100

    scroll menu

--- gym-exercises/src/components/BodyPart.jsx
+++ gym-exercises/src/components/BodyPart.jsx
@@ -12,12 +12,12 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         className='bodyPart-card'
         sx={{
             borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-                backgroundColor: '#fff',
-                borderBottomLeftRadius: '20px',
-                width: '270px',
-                height: '280px',
-                cursor: 'pointer',
-                gap: '47px'
+            backgroundColor: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor: 'pointer',
+            gap: '47px'
             }}
     >
         <img src={icon} alt="dumbell" style={{width: '40px', height: '40px'}} />
