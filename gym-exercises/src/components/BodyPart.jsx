9f1f823a7c8d59170c6b53578d274fc58bd25d6f
Eleshin <awwaleleshin@gmail.com>
2024-07-06 23:15:42 +0100

    styling

--- gym-exercises/src/components/BodyPart.jsx
+++ gym-exercises/src/components/BodyPart.jsx
@@ -20,7 +20,8 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
             gap: '47px'
             }}
     >
-        <img src={icon} alt="dumbell" style={{width: '40px', height: '40px'}} />
+        <img src={icon} alt="dumbell" 
+        style={{width: '40px', height: '40px'}} />
     </Stack>
   )
 }
