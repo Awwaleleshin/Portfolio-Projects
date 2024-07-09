794fd6ffd4d9e6c837e896d5ab1b2ab9db0ee083
Eleshin <awwaleleshin@gmail.com>
2024-07-09 16:08:42 +0100

    body part components

--- gym-exercises/src/components/BodyPart.jsx
+++ gym-exercises/src/components/BodyPart.jsx
@@ -10,18 +10,17 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         alignContent='center'
         alignItems='center'
         className='bodyPart-card'
-        sx={{
-            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
-            backgroundColor: '#fff',
-            borderBottomLeftRadius: '20px',
-            width: '270px',
-            height: '280px',
-            cursor: 'pointer',
-            gap: '47px'
+        sx={
+          bodyPart === item ? 
+          { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : 
+          { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+            onClick={() => {
+              setBodyPart(item)
             }}
     >
         <img src={icon} alt="dumbell" 
           style={{width: '40px', height: '40px'}} />
+          
           <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
     </Stack>
   )
